b390bfd0bc52a34c9adfd2c362d2821f501f4761
Jeffrey Stevens <47906337+jeffrey-stevens@users.noreply.github.com>
2024-11-23 17:29:37 -0500

    Created main test entry point.

--- sampling-wo-replacement/TODO.md
+++ sampling-wo-replacement/TODO.md
@@ -1,5 +1,7 @@
 # Todo
 
+* Create a README
+
 ## Enhancements
 
 * Make the distribution of random numbers truely uniform.
--- sampling-wo-replacement/sample.h
+++ sampling-wo-replacement/sample.h
@@ -1,6 +1,11 @@
 #ifndef SAMPLE_H
 #define SAMPLE_H
 
+#define ERR_SUCCESS             0
+#define ERR_MIN_GT_MAX          1
+#define ERR_RANGE_TOO_LARGE     2
+
+
 int gen_sample(int sample_size, int min_int, int max_int, int * sample);
 
 #endif
