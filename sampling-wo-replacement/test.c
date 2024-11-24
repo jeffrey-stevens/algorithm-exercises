70437350ad9291b47318c91cef107b90568f32de
Jeffrey Stevens <47906337+jeffrey-stevens@users.noreply.github.com>
2024-11-23 20:01:53 -0500

    Created test for when the number range = 1.  gen_sample() now prevents the sample size from being greater than the range.  gen_sample() now uses incremental probing rather than random regeneration to handle collisions.

--- sampling-wo-replacement/test.c
+++ sampling-wo-replacement/test.c
@@ -9,22 +9,128 @@
 #define MAX_NUM 9
 
 
-int main(int argc, char * argv[]) {
+static void fatal_error(char * message) {
+    perror(message);
+    exit(EXIT_FAILURE);
+}
 
-    int sampling[SAMPLE_SIZE];
-    int result = gen_sample(SAMPLE_SIZE, MIN_NUM, MAX_NUM, sampling);
 
-    printf("Return code: %d\n", result);
+int * samples_array(int size) {
+    
+    if (size < 0) {
+        fatal_error("Array size parameter was negative.");
+    }
+
+    int * samples = (int *) malloc(size * sizeof(int));
+
+    if (samples == NULL) {
+        fatal_error("Unable to allocate memory for samples array.");
+    }
+
+    return samples;
+}
+
+
+void print_samples(int sample_size, int * samples) {
+
+    printf("[");
+    for (int i = 0; i < sample_size; ++i) {
+        printf("%d", samples[i]);
+        if (i < sample_size - 1) {
+            printf(", ");
+        }
+    }
+    printf("]\n");
+
+    return;
+}
+
+
+bool test_min_eq_max() {
+    int min_int = 10;
+    int max_int = min_int;
+    int sample_size = 1;
+    int test_passed = false;
+
+    int * samples = samples_array(sample_size);
+
+    int result = gen_sample(sample_size, min_int, max_int, samples);
+
+    printf("Testing that min_int = max_int does not error and "
+            "generates only a single value...\n");
 
     if (result == ERR_SUCCESS) {
+        printf("The function did not error.\n");
 
-        printf("Result: [");
-        for (int i = 0; i < SAMPLE_SIZE; ++i) {
-            printf("%d", sampling[i]);
-            if (i < SAMPLE_SIZE - 1) {
-                printf(", ");
-            }
+        // Check that the correct value was written
+        if (samples[0] == min_int) {
+            printf("A single value of min_int = max_int = %d was generated.\n", min_int);
+            test_passed = true;
+        } else {
+            printf("The generated value of %d is not equal to min_int/max_int value of %d:\n",
+                samples[0], min_int);
+            print_samples(sample_size, samples);
+            test_passed = false;
         }
-        printf("]\n");
+
+    } else {
+        printf("The function returned error code %d.\n", result);
+        test_passed = false;
     }
+
+    free(samples);
+
+    return test_passed;
+}
+
+
+bool test_min_gt_max() {
+    return true;
+}
+
+
+bool test_range() {
+    return true;
+}
+
+bool test_sample_size() {
+    return true;
+}
+
+
+bool test_basic_usage() {
+
+    bool test_passed = false;
+    int sample_size = 10;
+    int * samples = samples_array(sample_size);
+    int min_num = 0;
+    int max_num = 9;
+
+    printf("Simple test of gen_sample().\n");
+
+    int result = gen_sample(sample_size, min_num, max_num, samples);
+
+    if (result == ERR_SUCCESS) {
+        printf("The array of samples was generated successfully:\n");
+        print_samples(SAMPLE_SIZE, samples);
+        test_passed = true;
+
+    } else {
+        printf("The array of samples failed with return code %d.\n", result);
+        test_passed = false;
+    }
+
+    free(samples);
+
+    return test_passed;
+}
+
+int main(int argc, char * argv[]) {
+
+    // TODO: Keep track of whether all tests passed or failed.
+    test_basic_usage();
+    printf("\n");
+    test_min_eq_max();
+
+    return EXIT_SUCCESS;
 }
