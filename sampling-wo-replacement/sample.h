050e2736255a1c9d87823b6db9d5aa2e5bb031b5
Jeffrey Stevens <47906337+jeffrey-stevens@users.noreply.github.com>
2024-11-23 23:05:03 -0500

    Created a more detailed test plan.  gen_samples() now catches negative sample sizes and NULL pointers.

--- sampling-wo-replacement/sample.h
+++ sampling-wo-replacement/sample.h
@@ -2,9 +2,11 @@
 #define SAMPLE_H
 
 #define ERR_SUCCESS                 0
-#define ERR_MIN_GT_MAX              1
-#define ERR_RANGE_TOO_LARGE         2
-#define ERR_SAMPLE_SIZE_TOO_LARGE   3
+#define ERR_NULL_ARRAY_POINTER      1
+#define ERR_NEG_SAMPLE_SIZE         2
+#define ERR_MIN_GT_MAX              3
+#define ERR_RANGE_TOO_LARGE         4
+#define ERR_SAMPLE_SIZE_TOO_LARGE   5
 
 
 int gen_sample(int sample_size, int min_int, int max_int, int * sample);
