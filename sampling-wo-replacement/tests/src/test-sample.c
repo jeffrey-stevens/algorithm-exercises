e897f6c007ce347d18c1ba8b9e7268f6c9c584bf
Jeffrey Stevens <47906337+jeffrey-stevens@users.noreply.github.com>
2024-11-27 21:02:38 -0500

    Moved the gen_sample() min_eq_max test to test-sample.c.

--- sampling-wo-replacement/tests/src/test-sample.c
+++ sampling-wo-replacement/tests/src/test-sample.c
@@ -9,4 +9,23 @@
 #include <criterion/new/assert.h>
 
 #include "sample.h"
+#include "utils.h"
 
+
+Test(gen_sample, min_eq_max, .description = 
+    "Test that min_int = max_int generates min_int/max_int." ) {
+
+    int min_int = 10;
+    int max_int = min_int;
+    int sample_size = 1;
+    int test_passed = false;
+
+    int * samples = int_array(sample_size);
+
+    int result = gen_sample(sample_size, min_int, max_int, samples);
+
+    cr_assert(eq(int, result, ERR_SUCCESS));
+    cr_expect(eq(int, samples[0], min_int));
+
+    free(samples);
+}
